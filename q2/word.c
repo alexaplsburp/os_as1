0740a4406bd72e9954db5be5324918ded5f651f4
aayush <aayush22011@iiitd.ac.in>
2023-08-29 19:27:42 +0530

    final commit

--- q2/word.c
+++ q2/word.c
@@ -1,114 +1,129 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <time.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s [-d STRING] [-R] file_name\n", argv[0]);
-        return 1;
+#define MAX_FILENAME_LENGTH 256
+#define MAX_BUFFER_SIZE 1024
+
+// Function to count words in a file
+int countWordsInFile(const char *filename, int ignoreNewline) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        fprintf(stderr, "Error: Unable to locate the file '%s'\n", filename);
+        return -1;
     }
 
-    char *file_name = NULL;
-    char *string_arg = NULL;
-    int useRFC5322Format = 0;
+    int wordCount = 0;
+    int insideWord = 0;
+    char currentChar;
+
+    while ((currentChar = fgetc(file)) != EOF) {
+        if (ignoreNewline && currentChar == '\n') {
+            continue;
+        }
+
+        if (currentChar == ' ' || currentChar == '\t' || currentChar == '\n') {
+            if (insideWord) {
+                insideWord = 0;
+            }
+        } else {
+            if (!insideWord) {
+                wordCount++;
+                insideWord = 1;
+            }
+        }
+    }
+
+    fclose(file);
+    return wordCount;
+}
+
+int main(int argc, char *argv[]) {
+    int ignoreNewlineFlag = 0;
+    int calculateDifferenceFlag = 0;
+    char *file1Path = NULL;
+    char *file2Path = NULL;
+    
+    int wordCount1; // Store the word count of the first file
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s [-n] [-d] file1 [file2]\n", argv[0]);
+        return 1;
+    }
 
     for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-d") == 0) {
-            if (i + 1 < argc) {
-                string_arg = argv[i + 1];
-                i++; // Skip the next argument
-            } else {
-                fprintf(stderr, "Error: -d option requires a STRING argument.\n");
-                return 1;
-            }
-        } else if (strcmp(argv[i], "-R") == 0) {
-            useRFC5322Format = 1;
+        if (strcmp(argv[i], "-n") == 0) {
+            ignoreNewlineFlag = 1;
+        } else if (strcmp(argv[i], "-d") == 0) {
+            calculateDifferenceFlag = 1;
         } else {
-            if (file_name == NULL) {
-                file_name = argv[i];
+            if (!file1Path) {
+                file1Path = argv[i];
+            } else if (!file2Path) {
+                file2Path = argv[i];
             } else {
-                fprintf(stderr, "Error: Only one file_name is allowed.\n");
+                fprintf(stderr, "Usage: %s [-n] [-d] file1 [file2]\n", argv[0]);
                 return 1;
             }
         }
     }
 
-    if (file_name == NULL) {
-        fprintf(stderr, "Error: Please provide a file_name.\n");
+    if (!file1Path) {
+        fprintf(stderr, "Error: Missing file1 argument\n");
         return 1;
     }
 
-    struct stat file_info;
-    if (stat(file_name, &file_info) != 0) {
-        perror("Error");
+    // Create a pipe to communicate the word count from child to parent
+    int pipefd[2];
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
         return 1;
     }
 
-    pid_t child_pid;
-    int status;
+    // Use fork to create a child process
+    pid_t childProcessID = fork();
 
-    child_pid = fork();
-
-    if (child_pid == -1) {
-        perror("Fork failed");
+    if (childProcessID < 0) {
+        fprintf(stderr, "Forking failed\n");
         return 1;
     }
 
-    if (child_pid == 0) {
+    if (childProcessID == 0) {
         // Child process
-        if (string_arg != NULL) {
-            FILE *file = fopen(file_name, "r");
-            if (file == NULL) {
-                perror("Error");
-                exit(1);
-            }
+        close(pipefd[0]); // Close the read end of the pipe in the child
 
-            char line[512];
-            int found = 0;
-            time_t last_modification_time = file_info.st_mtime;
-
-            while (fgets(line, sizeof(line), file)) {
-                if (strstr(line, string_arg)) {
-                    found = 1;
-                    last_modification_time = file_info.st_mtime;
-                    break;
-                }
-            }
-
-            fclose(file);
-
-            if (found) {
-                struct tm *time_info;
-                time_info = localtime(&last_modification_time);
-
-                char time_buffer[100];
-                strftime(time_buffer, sizeof(time_buffer), "%a %b %d %H:%M:%S %Z %Y", time_info);
-                printf("%s\n", time_buffer);
-            } else {
-                printf("String not found in the file.\n");
-            }
-        } else {
-            if (useRFC5322Format) {
-                char time_buffer[100];
-                strftime(time_buffer, sizeof(time_buffer), "%a, %d %b %Y %H:%M:%S %z", localtime(&file_info.st_mtime));
-                printf("%s\n", time_buffer);
-            } else {
-                // Display the last modification time in a custom format when no options are provided.
-                char time_buffer[100];
-                strftime(time_buffer, sizeof(time_buffer), "%a %b %d %H:%M:%S %Z %Y", localtime(&file_info.st_mtime));
-                printf("%s\n", time_buffer);
-            }
+        wordCount1 = countWordsInFile(file1Path, ignoreNewlineFlag);
+        if (wordCount1 >= 0) {
+            // Send the word count to the parent through the pipe
+            write(pipefd[1], &wordCount1, sizeof(wordCount1));
         }
 
+        close(pipefd[1]); // Close the write end of the pipe in the child
         exit(0);
     } else {
         // Parent process
-        wait(&status);
+        close(pipefd[1]); // Close the write end of the pipe in the parent
+
+        int status;
+        waitpid(childProcessID, &status, 0);
+
+        // Read the word count from the child through the pipe
+        read(pipefd[0], &wordCount1, sizeof(wordCount1));
+        close(pipefd[0]); // Close the read end of the pipe in the parent
+
+        if (wordCount1 >= 0) {
+            printf("The file '%s' contains %d words.\n", file1Path, wordCount1);
+        }
+
+        if (file2Path && calculateDifferenceFlag) {
+            int wordCount2 = countWordsInFile(file2Path, ignoreNewlineFlag);
+            if (wordCount2 >= 0) {
+                int wordCountDifference = abs(wordCount1 - wordCount2);
+                printf("Word count difference between '%s' and '%s': %d\n", file1Path, file2Path, wordCountDifference);
+            }
+        }
     }
 
     return 0;
